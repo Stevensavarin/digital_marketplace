d8c30d0a995f7542f270f0a8cbf5afd60313d8e0
steven_savarin <srssdesing@gmail.com>
2024-02-04 00:59:36 +0100

    test

--- dist/config/index.js
+++ dist/config/index.js
@@ -3,44 +3,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.PRODUCT_CATEGORIES = void 0;
 exports.PRODUCT_CATEGORIES = [
     {
-        label: "UI Kits",
-        value: "ui_kits",
+        label: 'UI Kits',
+        value: 'ui_kits',
         featured: [
             {
-                name: "Editor picks",
-                href: "#",
-                imageSrc: "/nav/ui-kits/mixed.jpg"
+                name: 'Editor picks',
+                href: "/products?category=ui_kits",
+                imageSrc: '/nav/ui-kits/mixed.jpg',
             },
             {
-                name: "New Arrivals",
-                href: "#",
-                imageSrc: "/nav/ui-kits/blue.jpg",
+                name: 'New Arrivals',
+                href: '/products?category=ui_kits&sort=desc',
+                imageSrc: '/nav/ui-kits/blue.jpg',
             },
             {
-                name: "Bestsellers",
-                href: "#",
-                imageSrc: "/nav/ui-kits/purple.jpg",
+                name: 'Bestsellers',
+                href: '/products?category=ui_kits',
+                imageSrc: '/nav/ui-kits/purple.jpg',
             },
         ],
     },
     {
-        label: "Icons",
-        value: "icons",
+        label: 'Icons',
+        value: 'icons',
         featured: [
             {
-                name: "Favorite Icon Picks",
-                href: "#",
-                imageSrc: "/nav/icons/picks.jpg"
+                name: 'Favorite Icon Picks',
+                href: "/products?category=icons",
+                imageSrc: '/nav/icons/picks.jpg',
             },
             {
-                name: "New Arrivals",
-                href: "#",
-                imageSrc: "/nav/icons/new.jpg",
+                name: 'New Arrivals',
+                href: '/products?category=icons&sort=desc',
+                imageSrc: '/nav/icons/new.jpg',
             },
             {
-                name: "Bestselling Icons",
-                href: "#",
-                imageSrc: "/nav/icons/bestsellers.jpg",
+                name: 'Bestselling Icons',
+                href: '/products?category=icons',
+                imageSrc: '/nav/icons/bestsellers.jpg',
             },
         ],
     },
